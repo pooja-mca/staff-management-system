b97036a6d05a80ce8e5dfae2f18b741d1f437541
pooja <kushwahapooja038@gmail.com>
2019-08-18 18:16:50 +0530

    Update db seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,9 +12,9 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Gbeila Aliu Wahab',
-            'email' => 'aliuwahab@gmail.com',
-        	'password' => bcrypt('secret'),
+            'name' => 'Kushwaha Pooja',
+            'email' => 'admin@gmail.com',
+        	'password' => bcrypt('admin123'),
         	'is_admin' => true,
             'created_at' => \Carbon\Carbon::now(),
             'updated_at' => \Carbon\Carbon::now(),
@@ -22,9 +22,9 @@ class UsersTableSeeder extends Seeder
 
 
         DB::table('users')->insert([
-            'name' => 'Faiq Eshan Aliu',
+            'name' => 'devangi vasara',
             'email' => 'admin@site.com',
-            'password' => bcrypt('secret'),
+            'password' => bcrypt('admin123'),
             'is_admin' => false,
             'created_at' => \Carbon\Carbon::now(),
             'updated_at' => \Carbon\Carbon::now(),
